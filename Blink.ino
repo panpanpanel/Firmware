1498df7e3cc4b1f0c140adb942d3fcba631d8468
AnshJuneja <ansh.juneja98@gmail.com>
2022-03-23 22:03:08 -0400

    Pre-Video Code

--- Blink.ino
+++ Blink.ino
@@ -36,6 +36,23 @@ void messageHandler(String &topic, String &payload) {
   //Serial.println(breaker);
 }
 
+void setupMuxCh1Input() {
+  pinMode(25, OUTPUT);
+  pinMode(26, OUTPUT);
+  pinMode(33, OUTPUT);
+  pinMode(33, OUTPUT);
+  
+  digitalWrite(25, HIGH);
+  digitalWrite(26, HIGH);
+  digitalWrite(32, LOW);
+  digitalWrite(33, LOW);
+}
+
+setupClearCurrentPin() {
+  pinMode(clearCurrentPin, OUTPUT);
+  digitalWrite(clearCurrentPin, LOW);
+}
+
 void IRAM_ATTR isr1() {
   time1 = esp_timer_get_time();
   done_1 = true;
@@ -77,15 +94,16 @@ int getPhaseDifference () { // THIS MIGHT NEED TO BE VOLATILE INT BECAUSE 'DIFF'
   return local_diff;
 }
 
-int getBreakerPositionInCm (int breaker) {
+// returns breaker position in mm
+int getBreakerPosition (int breaker) {
   if (breaker == 1) {
-    return 25;
+    return 10;
   }
   if (breaker == 2) {
-    return 2;
+    return 60;
   }
   if (breaker == 3) {
-    return 3;
+    return 110;
   }
 }
 
@@ -96,55 +114,21 @@ void adjustFlipList () {
 }
 
 void setup() {
-  //delay(5000);
   Serial.begin(115200);
-
-  Serial.println ("1");
-
-  pinMode(clearCurrentPin, OUTPUT);
-  digitalWrite(clearCurrentPin, LOW);
-
-  Serial.println ("2");
-
-  pinMode(25, OUTPUT);
-  pinMode(26, OUTPUT);
-  pinMode(33, OUTPUT);
-  pinMode(33, OUTPUT);
-
-  Serial.println ("3");
-
-  digitalWrite(25, HIGH);
-  digitalWrite(26, HIGH);
-  digitalWrite(32, LOW);
-  digitalWrite(33, LOW);
-
-  Serial.println ("4");
-
+  setupClearCurrentPin();
+  setupMuxCh1Input();
   startSPI();
-
-  Serial.println ("5");
-  
   connectToCloud();
-
   setupADC();
-
-  Serial.println ("6");
-
   setupServos();
-
-  Serial.println ("7");
-
   setupStepper();
-
-  Serial.println ("8");
-
   homeStepper();
-  
   //setupPhaseDifferenceMeasurement ();
 }
 
 void loop() {
   int stepperPosition = getActualStepperPosition();
+  
   //Serial.print ("Actual Stepper Position: ");
   //Serial.println (getActualStepperPosition());
   //delay(500);
@@ -152,41 +136,14 @@ void loop() {
   // spaces between breakers are 5 cm
 
   if (flipListSize > 0) {
-    setTargetStepperPosition(flipListSize);
-    Serial.print ("Actual Stepper Position: ");
-    Serial.println (stepperPosition);
-    //flipBreakerA();
-    //flipListSize--;
+    setTargetStepperPosition(getBreakerPosition(flipList[0]));
+    //Serial.print ("Actual Stepper Position: ");
+    //Serial.println (stepperPosition);
     
-    //Serial.print ("test ");
-    //Serial.println (getBreakerPositionInCm(flipList[0]));
-    //setTargetStepperPosition(getBreakerPositionInCm(flipList[0]));
-    //if (getActualStepperPosition() == getBreakerPositionInCm(flipList[0])) {
-    if (stepperPosition == flipListSize) {
+    if (stepperPosition == getBreakerPosition(flipList[0])) {
       flipBreakerA();
-      delay (500);
-      flipBreakerA();
-      delay (500);
-      //adjustFlipList(); // shift elements in flip list to the left
-      //flipListSize--;
-    }
-  }
-  else {
-    //byte WRITE_ZERO_ACTUAL_POSITION[4] = {0b10, 0, 0, 0};
-    //dataTransfer (WRITE_ZERO_ACTUAL_POSITION);
-    //Serial.print ("not flip ");
-    //Serial.println (getActualStepperPosition());
-  }
-
-  if (millis() > 13000) {
-    if (flipListSize == 0){
-      flipListSize = 10;
-    }
-    else if (flipListSize == 10 && stepperPosition == 10) {
-      flipListSize = 60;
-    }
-    else if (flipListSize == 60 && stepperPosition == 60) {
-      flipListSize = 0;
+      adjustFlipList();
+      flipListSize--;
     }
   }
   
@@ -199,13 +156,12 @@ void loop() {
   Serial.print ("ADC Reading: ");
   Serial.println (breaker1Current);
   
-  //float currents[8] = {1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5};
-  //currents[0] = breaker1Current;
-  //publishMessage(1, 120, currents);
+  float currents[8] = {1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5};
+  currents[0] = breaker1Current;
+  publishMessage(1, 120, currents);
 
-  //client.loop();
+  client.loop();
   //delay(50);
-  
   
   /*
   if (Serial.available() > 0) {
